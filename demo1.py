3875776fd44e7b0fda1dfdc280c2069c791026d2
Atiahh <106502875+Atiahh@users.noreply.github.com>
2022-06-09 11:32:00 +0800

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -28,7 +28,7 @@ st.subheader('User Input parameters')
 st.write(df)
 
 iris = pd.read_csv('https://raw.githubusercontent.com/Atiahh/finalproject/main/IRIS.csv')
-X = iris.DataFrame(column=['sepal_length','sepal_width','petal_length','petal_width'], axis=1)
+X = iris.iloc[['sepal_length','sepal_width','petal_length','petal_width'], axis=1)
 Y = iris.species
 
 clf = RandomForestClassifier()
